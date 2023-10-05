0080974400b551e96f48d15590c0e318d1fc6e31
Luis Mendoza <hyh@hotmail.com>
2023-10-05 14:15:29 -0500

    Monorepo Nx Angular

--- apps/landing/src/app/app.module.ts
+++ apps/landing/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { LayoutModule } from './layout/layout.module';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ImagesComponent } from './components/images/images.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { TopicsComponent } from './components/topics/topics.component';
+import { LevelComponent } from './components/level/level.component';
+import { BannerComponent } from './components/banner/banner.component';
+
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ImagesComponent,
+    DescriptionComponent,
+    TopicsComponent,
+    LevelComponent,
+    BannerComponent,
+  ],
+  imports: [BrowserModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
